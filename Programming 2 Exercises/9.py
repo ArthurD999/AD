1eafd08d5efa880476462980ad62fbdea635e6d9
Arthur <21ADrummond.ACC@lwetb.ie>
2025-10-18 10:53:45 +0100

    Refactor water bill calculation with elif statements

--- Programming 2 Exercises/9.py
+++ Programming 2 Exercises/9.py
@@ -5,9 +5,10 @@ units_used=int(input("Enter how many units of water were used:"))
 if units_used < 100:
     band_1 = (0.05*units_used)
     print(band_1,"$")
-if units_used >100 and units_used <250:
+elif units_used >100 and units_used <250:
     band_1 = ((units_used-100)*0.1)+0.05*100
     print(band_1,"$")
-if units_used > 250:
+elif units_used > 250:
     band_1 = ((units_used-250)*0.2) + 100*0.05+0.1*150
     print("Water bill is",band_1,"$")
+
